d446648a823c81612019e8a92c467d0e8d1709d2
PRITAM DAS <er.pritamdas22@gmail.com>
2021-12-17 23:00:15 +0530

    Type Conversions

--- Type_Conversions/A-Basic Type TO Class Type.cpp
+++ Type_Conversions/A-Basic Type TO Class Type.cpp
@@ -4,12 +4,26 @@ using namespace std;
 
 class BasicTOClass
 {
-    
+    private:
+        int hrs;
+        int mins;
+    public:
+        BasicTOClass(){ }
+        BasicTOClass(int duration);
+        void display();
 };
+
+BasicTOClass :: BasicTOClass(int duration)
+{
+
+}
 int main()
 {
     system("cls");
 
+    BasicTOClass time1;
+    int duration;
+    time1=duration;
     getch();
     return 0;
 }
